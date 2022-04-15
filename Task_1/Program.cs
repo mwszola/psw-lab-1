35e228119883388d01ff058ffd4077d991bf2965
Mateusz Wszola <matwsz000@utp.edu.pl>
2022-04-15 13:06:00 +0200

    Added comments

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,10 +1,13 @@
 ﻿var generateNumber = new Random();
 
+// Wylosowanie dodatniej liczby mniejszej od 10 
 var randomNumber = generateNumber.Next(10);
 var userPick = -1;
 
+// Dopoki uzytkownik nie zgadnie liczby 
 while (userPick != randomNumber) {
     Console.WriteLine("Podaj liczbe");
+    // Pobieramy dane od uzytkownika, ktore konwertujemy na liczbe 
     userPick = Convert.ToInt32(Console.ReadLine());
     if (userPick > randomNumber) {
         Console.WriteLine("Liczba jest wieksza od X");
@@ -14,8 +17,3 @@ while (userPick != randomNumber) {
         Console.WriteLine("Zgadles!");
     }
 }
-
-
-
-
-
--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,15 +1,19 @@
-﻿int[] userPicks = new int[10];
+﻿// Inicjalizacja 10-elementowej tablicy
+int[] userPicks = new int[10];
 
 var uniqueNumbers = 0;
 
-while (true)
+for (var i = 0; i < 10; i++)
 {
     Console.WriteLine("Podaj liczbe");
+    // Pobranie danych od uzytkownika i konwersja na liczbe
     var userPick = Convert.ToInt32(Console.ReadLine());
+    // Jezeli tablica nie zawiera jeszcze takiej liczby
     if (!userPicks.Contains(userPick))
     {
+        // Dodajemy liczbe do tablicy i zwiekszamy ilosc unikatowych wartosci
         userPicks[uniqueNumbers++] = userPick;
     }
-
+    // Co kazda interacje wyswietlamy ilosc unikatowych wartosci do tej pory
     Console.WriteLine($"Liczba unikatowych wartosci: {uniqueNumbers}");
 }
